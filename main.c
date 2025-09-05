c3646c43b21946bb22b4d081272e29975c7214e4
ee25mt011 <ee25mt011@iitdh.ac.in>
2025-09-05 20:58:57 +0530

    adding expt2a

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
+
+int main(void) {
+
+SYSCTL_RCGC2_R = 0x20;;
+GPIO_PORTF_LOCK_R = 0x4C4F434B;
+GPIO_PORTF_CR_R = 0x1F;
+GPIO_PORTF_DIR_R = 0x0E;
+GPIO_PORTF_DEN_R = 0x1F;
+GPIO_PORTF_PUR_R = 0x11;
+
+while(1){
+if((GPIO_PORTF_DATA_R & 0x01)==0){
+    GPIO_PORTF_DATA_R = 0x02;
+}
+else{
+    GPIO_PORTF_DATA_R &=~0x02;
+}
+
+if((GPIO_PORTF_DATA_R & 0x10)==0){
+    GPIO_PORTF_DATA_R = 0x04;
+    }
+    else{
+    GPIO_PORTF_DATA_R &=~0x04;
+    }
+}
+}
+
+
