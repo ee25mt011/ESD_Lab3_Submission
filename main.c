a9c453148f8fc91c15df106326aff4413e286e17
ee25mt011 <ee25mt011@iitdh.ac.in>
2025-09-20 23:13:58 +0530

    Lab_6 code is added

--- main.c
+++ main.c
@@ -1,47 +1,29 @@
 #include <stdint.h>
+#include "tm4c123gh6pm.h"
 
-#define SYSCTL_RCGC2_R      (*((volatile uint32_t *)0x400FE108))
-#define GPIO_PORTF_DATA_R   (*((volatile uint32_t *)0x400253FC))
-#define GPIO_PORTF_DIR_R    (*((volatile uint32_t *)0x40025400))
-#define GPIO_PORTF_DEN_R    (*((volatile uint32_t *)0x4002551C))
-#define GPIO_PORTF_LOCK_R   (*((volatile uint32_t *)0x40025520))
-#define GPIO_PORTF_CR_R     (*((volatile uint32_t *)0x40025524))
-#define GPIO_PORTF_PUR_R    (*((volatile uint32_t *)0x40025510))
-#define GPIO_LOCK_KEY       0x4C4F434B
+void delayMs(int n);
 
-#define STCTRL              (*((volatile uint32_t *)0xE000E010))
-#define STRELOAD            (*((volatile uint32_t *)0xE000E014))
-#define STCURRENT           (*((volatile uint32_t *)0xE000E018))
+int main(void)
+{
+    SYSCTL_RCGCGPIO_R = 0x20;
+    while((SYSCTL_PRGPIO_R & 0x20) == 0);
+    GPIO_PORTF_LOCK_R = 0x4C4F434B;
+    GPIO_PORTF_CR_R = 0x02;
+    GPIO_PORTF_DIR_R = 0x02;
+    GPIO_PORTF_DEN_R = 0x02;
+    while(1)
+    {
+        GPIO_PORTF_DATA_R = 0x02;
+        delayMs(500);
 
-#define RED_LED     (1 << 1)
-#define BLUE_LED    (1 << 2 )
-#define SW1         (1 << 4 )
-
-int main(void) {
-    SYSCTL_RCGC2_R |= 0x20;
-
-    GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;
-    GPIO_PORTF_CR_R |= (RED_LED | BLUE_LED | SW1);
-    GPIO_PORTF_DIR_R |= (RED_LED | BLUE_LED);
-    GPIO_PORTF_DIR_R &= ~ (SW1);
-    GPIO_PORTF_DEN_R |= (RED_LED | SW1 | BLUE_LED);
-    GPIO_PORTF_PUR_R |= (SW1);
-
-
-
-    STRELOAD = 16000000-1;
-    STCURRENT = 0;
-    STCTRL = (1 << 2) | (1 << 0);
-
-    while (1) {
-        if(STCTRL & (1 << 16)){
-        GPIO_PORTF_DATA_R ^= RED_LED;
-        }
-        if((GPIO_PORTF_DATA_R & SW1) == 0){
-                 GPIO_PORTF_DATA_R |= BLUE_LED;
-             }
-             else{
-                 GPIO_PORTF_DATA_R &= ~ BLUE_LED;
-         }
+        GPIO_PORTF_DATA_R = 0x00;
+        delayMs(500);
+    }
 }
+
+void delayMs(int n)
+{
+    int i, j;
+    for(i = 0; i < n; i++)
+        for(j = 0; j < 3180; j++);
 }
